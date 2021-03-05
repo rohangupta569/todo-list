06a2c5baf3b95457104d32504335f33eb27f03ca
Rohan Gupta <rohangupta569@gmail.com>
2021-03-05 16:37:22 +0530

    change in task-category color with task-category as id and using querySelectorAll in home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,5 @@
 //getting the array of the task category elements
-var elements = document.getElementsByClassName('task-category');
+var elements = document.querySelectorAll('#task-category');
 
 //Changing the color of the elements regarding there task category
 for(var i=0 ;i < elements.length ; i++)
